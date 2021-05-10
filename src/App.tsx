edf24cc981576edb9f6e18682a8ffb3f2d2a01f0
Melissa Astbury <melissaastbury@hotmail.com>
2021-05-10 15:39:40 +0100

    ensured the state holds the form input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import './App.css';
+import { IRecipe } from './IRecipe';
+import RecipeComponent from './RecipeComponent';
 
 const App: React.FC = () => {
-  const [recipeFound, setRecipeFound] = useState([]);
+  const [recipesFound, setRecipesFound] = useState<IRecipe[]>([]);
   const [recipeSearch, setRecipeSearch] = useState('');
 
-  const searchForRecipes = async (query: string): Promise<any> => {
+  const searchForRecipes = async (query: string): Promise<IRecipe[]> => {
     const result = await fetch(`http://localhost:3001/?search=${query}`);
     return (await result.json()).results;
   };
@@ -15,23 +17,34 @@ const App: React.FC = () => {
     const form = event.target as HTMLFormElement;
     const input = form.querySelector('#searchText') as HTMLInputElement;
     setRecipeSearch(input.value);
+    input.value = '';
   };
 
   useEffect(() => {
     (async () => {
       const query = encodeURIComponent(recipeSearch);
       const response = await searchForRecipes(query);
-      setRecipeFound(response);
+      setRecipesFound(response);
     })();
   }, [recipeSearch]);
 
   return (
     <div className="App">
       <h1>Recipe Search App</h1>
-      <form className="searchForm">
+      <form className="searchForm" onSubmit={(event) => search(event)}>
         <input type="text" id="searchText" />
         <button>Search</button>
       </form>
+      {recipeSearch && <p>Result for {recipeSearch}...</p>}
+      <div>
+        {recipesFound.length &&
+          recipesFound.map((recipe) => (
+            <RecipeComponent
+              key={recipe.href}
+              recipe={recipe}
+            ></RecipeComponent>
+          ))}
+      </div>
     </div>
   );
 };
